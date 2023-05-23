9c37b510709d81cac5fa9acd68ff8cf4ed387fd6
Jeong Jeongil <jji042842@gmail.com>
2023-05-23 12:53:05 +0900

    배치 기본 테스트 완료

--- src/main/java/com/jeongil/autotrading/service/trading/AutoTradingServiceImpl.java
+++ src/main/java/com/jeongil/autotrading/service/trading/AutoTradingServiceImpl.java
@@ -1,5 +1,7 @@
 package com.jeongil.autotrading.service.trading;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -8,9 +10,10 @@ import java.text.ParseException;
 @Service
 public class AutoTradingServiceImpl implements AutoTradingService {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
     public void process() throws ParseException, IOException {
-
+        logger.info("배치 돌았습니다요~");
     }
 }
